389e930a77f178c6a25983bdfa5ce336c4431bc3
Roland Ernotte <rol@rern.be>
2020-11-17 13:58:17 +0100

    index.js simplification

--- www/js/index.js
+++ www/js/index.js
@@ -1,19 +1,11 @@
-var app = {
-    
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+document.addEventListener('deviceready', onDeviceReady, false);
 
-    // gestionnaire d'évènement deviceready 
-    onDeviceReady: function() {
-        // initialisation de Framework7
-		var f7App = new Framework7({
-            root: '#app'
-        });
-		// initialisation de la vue principale
-        var mainView = f7App.views.create('.view-main');
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
 
-    }
-};
-
-app.initialize();
+    var f7App = new Framework7({
+		root: '#app'
+	})
+	
+	var mainView = f7App.views.create('.view-main');
+}
